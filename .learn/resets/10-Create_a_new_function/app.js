4e077f134768fe0a044cc3aeac1627b9b9fa4432
marellanorero <marellanonorero@gmail.com>
2022-05-08 13:12:08 +0000

    avanzando

--- .learn/resets/10-Create_a_new_function/app.js
+++ .learn/resets/10-Create_a_new_function/app.js
@@ -1,8 +1,3 @@
 /* This exercise is almost identical to 15-Random numbers - consider merging them here and using another concept for 15? */
 
 // Declare your function here
-function generateRandom(min, max) {
-    return Math.floor((Math.random() * (9 - 0 + 1)) + 0);
-}
-
-console.log(generateRandom())
